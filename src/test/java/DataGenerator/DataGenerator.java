4e1b72c39142e554d352b83c4908329eaebf5c99
DMITRI4IVANOV <DmitryIvanovAN@yandex.ru>
2022-08-28 19:50:11 +0400

    Test fail

--- src/test/java/DataGenerator/DataGenerator.java
+++ src/test/java/DataGenerator/DataGenerator.java
@@ -1,18 +1,24 @@
 package DataGenerator;
 
+import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.var;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.util.Locale;
+
+import static DataGenerator.DataGenerator.AuthTest.makeRequest;
 import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
     private DataGenerator() {
     }
+
     // спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
-    class AuthTest {
+    public static class AuthTest {
         private static RequestSpecification requestSpec = new RequestSpecBuilder()
                 .setBaseUri("http://localhost")
                 .setPort(9999)
@@ -21,16 +27,39 @@ public class DataGenerator {
                 .log(LogDetail.ALL)
                 .build();
 
-        @BeforeAll
-        static void setUpAll() {
+
+        static void makeRequest(UserData userInfo) {
             // сам запрос
             given() // "дано"
                     .spec(requestSpec) // указываем, какую спецификацию используем
-                    .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                    .body(userInfo) // передаём в теле объект, который будет преобразован в JSON
                     .when() // "когда"
                     .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
                     .then() // "тогда ожидаем"
                     .statusCode(200); // код 200 OK
         }
     }
+
+    public static class Registration {
+        private static Faker faker = new Faker(new Locale("en"));
+
+        public static UserData generateUser(String status) {
+            var user = new UserData(faker.name().fullName(), faker.internet().password(), status);
+            makeRequest(user);
+            return user;
+        }
+
+        public static UserData generateWrongLoginUser(String status) {
+            var password = faker.internet().password();
+            makeRequest(new UserData(faker.name().firstName(), password, status));
+            return new UserData(faker.name().firstName(), password, status);
+        }
+
+        public static UserData generateWrongPasswordUser(String status) {
+            var login = faker.name().firstName();
+            makeRequest(new UserData(login, faker.internet().password(), status));
+            return new UserData(login, faker.internet().password(), status);
+        }
+    }
 }
+
--- src/test/java/DataGenerator/UserData.java
+++ src/test/java/DataGenerator/UserData.java
@@ -1,5 +1,12 @@
 package DataGenerator;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserData {
     private String login;
     private String password;
